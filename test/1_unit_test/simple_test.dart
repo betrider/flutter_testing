9153604f205a9e2d9e470f06a7ad848f2c067244
betrider <tlsfpem@gmail.com>
2023-03-27 15:41:23 +0900

    merge

--- test/1_unit_test/simple_test.dart
+++ test/1_unit_test/simple_test.dart
@@ -1,6 +1,33 @@
+// ignore_for_file: avoid_print
+
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test('call Faker', () {
+    var faker = Faker();
+
+    print(faker.internet.email());
+    // francisco_lebsack@buckridge.com
+
+    print(faker.internet.ipv6Address());
+    // 2450:a5bf:7855:8ce9:3693:58db:50bf:a105
+
+    print(faker.internet.userName());
+    // fiona-ward
+
+    print(faker.person.name());
+    // Fiona Ward
+
+    print(faker.person.prefix());
+    // Mrs.
+
+    print(faker.person.suffix());
+    // Sr.
+
+    print(faker.lorem.sentence());
+  });
+
   test('String.split() splits the string on the delimiter', () {
     var string = 'foo,bar,baz';
     expect(string.split(','), equals(['foo', 'bar', 'baz']));
@@ -31,5 +58,4 @@ void main() {
       expect(11.toRadixString(16), equals('b'));
     });
   });
-
 }
